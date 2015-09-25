2c33335d8e13b63d635745a9b374af0589fc364c
Christopher Weston <fogelmatrix@gmail.com>
2015-09-25 18:44:53 -0400

    Changed map markers to an AJAX request from a JSON file

--- squaretwo/js/square2.js
+++ squaretwo/js/square2.js
@@ -1,18 +1,37 @@
 var map;
 var centerMap = {lat:41.4836447, lng:-81.6511137};
-var latLong1 = {lat: 40.2319037, lng: -75.1366881};
-var latLong2 = {lat: 43.0340457, lng: -87.9094738};
-var latLong3 = {lat: 39.9636918, lng: -86.1153007};
+
+// ARRAYS
+var lats = new Array;
+var longs = new Array;
+var latLong = new Array;
 var infoWindow = new Array;
 var marker = new Array;
 var contentString = new Array;
 
-/******  info window data  ******/
-	contentString[0] = '<h4>Square 2 Corporate Headquarters</h4>';
-	contentString[1] = '<h4>Square 2- Milwaukee WI</h4>';
-	contentString[2] = '<h4>Square 2- Indianapolis IN</h4>';
 
-/******  set content of infowindows  ******/
+/******  JSON PARSER  ******/
+
+$.getJSON('data/locations.json', function(data){
+	
+	$.each(data, function(key, val){
+
+		contentString[key] = val.location;
+		lats[key] = val.lat;
+		longs[key] = val.long;
+		
+		// ASSIGN LATITUDE AND LONGITUDE
+		latLong[key]  = {lat: lats[key], lng: longs[key]};
+
+		return(contentString, lats, longs);
+	});
+
+}); // END JSON FUNCTION
+
+
+
+/******  SET CONTENT OF INFOWINDOWS  ******/
+
 
 function buildMarkers(){
 	for (q=0; q<=2; q++){
@@ -26,23 +45,22 @@ function buildMarkers(){
 /******  INITALIZE MAP  ******/
 
 function initMap() {
-
 	  map = new google.maps.Map(document.getElementById('map'), {
 		center: centerMap,
 		zoom: 6
 	  });
 	  marker[0] = new google.maps.Marker({
-		position: latLong1,
+		position: latLong[0],
 		map: map,
 		title: 'Square2- Corporate Headquarters'
 	  });
 	  marker[1] = new google.maps.Marker({
-		position: latLong2,
+		position: latLong[1],
 		map: map,
 		title: 'Square2- Milwaukee WI'
 	  });
 	  marker[2] = new google.maps.Marker({
-		position: latLong3,
+		position: latLong[2],
 		map: map,
 		title: 'Square2- Indianapolis IN'
 	  });
